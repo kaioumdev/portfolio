85b50cd518bc80da29603222462f8860405dbbab
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-01 20:00:14 +0600

    feat(router): implements router with this projects

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,47 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import Blogs from './pages/Blogs/Blogs.jsx';
+// import Resume from './pages/Resume/Resume.jsx';
+// import About from './pages/About/About.jsx';
+// import Contact from './pages/Contact/Contact.jsx';
+// import Projects from './pages/Projects/Projects.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      // {
+      //   path: "/",
+      //   element: <About/>,
+      // },
+      // {
+      //   path: "/resume",
+      //   element: <Resume/>,
+      // },
+      // {
+      //   path: "/projects",
+      //   element: <Projects/>,
+      // },
+      // {
+      //   path: "blogs",
+      //   element: <Blogs/>
+      // },
+      // {
+      //   path: "contact",
+      //   element: <Contact/>
+      // }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
