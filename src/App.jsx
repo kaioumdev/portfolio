66657c6d2f3a1bc9638fe092b6d8549e7a390da4
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-01 20:12:54 +0600

    feat(app): updated app file for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+/* eslint-disable react/no-unescaped-entities */
+
+import "./App.css";
+
+
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Aside from "./components/Aside";
 
 function App() {
-
   return (
     <>
-   
+      <Aside/>
+      <div className="main-content">
+       <Navbar/>
+        <section style={{
+          scrollBehavior: "smooth"
+        }}><Outlet/></section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
