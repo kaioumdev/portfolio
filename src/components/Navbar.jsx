216dfafa1ae98f8193fe4be2b5f875cb8767486b
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-21 03:13:05 +0600

    feat(resume): resume link redirec with navabar directly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,38 +3,38 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <ul className="navbar-list">
-      <li className="navbar-item">
-        <NavLink to="/" className="navbar-link" data-nav-link>
-          About
-        </NavLink>
-      </li>
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <NavLink to="/" className="navbar-link" data-nav-link>
+            About
+          </NavLink>
+        </li>
 
-      <li className="navbar-item">
-        <NavLink to="/resume" className="navbar-link" data-nav-link>
-          Resume
-        </NavLink>
-      </li>
+        <li className="navbar-item">
+          <a href="/resume.pdf" className="navbar-link" data-nav-link target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
+        </li>
 
-      <li className="navbar-item">
-        <NavLink to="/projects" className="navbar-link" data-nav-link>
-          Projects
-        </NavLink>
-      </li>
+        <li className="navbar-item">
+          <NavLink to="/projects" className="navbar-link" data-nav-link>
+            Projects
+          </NavLink>
+        </li>
 
-      <li className="navbar-item">
-        <NavLink to="/blogs" className="navbar-link" data-nav-link>
-          Blog
-        </NavLink>
-      </li>
+        <li className="navbar-item">
+          <NavLink to="/blogs" className="navbar-link" data-nav-link>
+            Blog
+          </NavLink>
+        </li>
 
-      <li className="navbar-item">
-        <NavLink to="/contact" className="navbar-link" data-nav-link>
-          Contact
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
+        <li className="navbar-item">
+          <NavLink to="/contact" className="navbar-link" data-nav-link>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
